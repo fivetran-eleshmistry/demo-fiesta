57827f9314a0ea4bc1336dc3f0f7f67b4b387131
fivetran-eleshmistry <62256075+fivetran-eleshmistry@users.noreply.github.com>
2020-06-01 22:14:56 +0100

    Update snowflakes3transform.sql

--- transformations/snowflakes3transform.sql
+++ transformations/snowflakes3transform.sql
@@ -1,1 +1,1 @@
-copy into s3://fivetran-se-snowflake-unload/cgroup/ from "ELESHM"."SALESFORCE_DEV"."LEAD" storage_integration = fivetranunloadtest;
+copy into s3://fivetran-se-snowflake-unload/cgroup/ from "ELESHM"."SALESFORCE_DEV"."LEAD" storage_integration = fivetranunloadtest1;
